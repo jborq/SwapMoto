edcd5359507a56bc4775fd12a618708442a4c730
Jakub Borkiewicz <kuba19102002@gmail.com>
2024-11-06 22:40:28 +0100

    Refactor index layout and improve styling for responsiveness

--- index.php
+++ index.php
@@ -14,40 +14,29 @@
                 <img src=".\public\images\SwapMoto.png" alt="Logo" />
             </div>
             <div class="content-up">
-                <p>6 Types of Motorcycles</p>
                 <p>Garages in 4 Cities</p>
             </div>
             <div class="content-down">
-                Types of Motorcycles:
-                <ul>
-                    <li>Chopper</li>
-                    <li>Cruiser</li>
-                    <li>Enduro</li>
-                    <li>Off-road</li>
-                    <li>Sport</li>
-                    <li>Touring</li>
-                </ul>
+                <p>6 Types of Motorcycles</p>
             </div>
         </div>
 
         <div class="content-middle">
             <h1>Find nearby garage in your city</h1>
             <div class="slideshow">
-                <div class="slide-container">
-                    <img src=".\public\images\mt-07.png" alt="Yamaha MT-07" class="slide active">
-                    <img src=".\public\images\triumph.png" alt="Triumph" class="slide">
-                    <img src=".\public\images\harley.png" alt="Harley" class="slide">
-                    <img src=".\public\images\fz6.png" alt="Yamaha FZ6" class="slide">
-                </div>
+                <img src=".\public\images\mt-07.png" alt="Yamaha MT-07" class="slide active">
+                <img src=".\public\images\triumph.png" alt="Triumph" class="slide">
+                <img src=".\public\images\harley.png" alt="Harley" class="slide">
+                <img src=".\public\images\fz6.png" alt="Yamaha FZ6" class="slide">
             </div>
             <div class="button">
-                <button type="button" onclick="location.href='login.php'">Get Started</button>
+                <button type="button" onclick="location.href='./public/login.php'">Get Started</button>
             </div>
         </div>
 
         <div class="content-right">
             <div class="button">
-                <button type="button" onclick="location.href='register.php'">Register</button>
+                <button type="button" onclick="location.href='./public/register.php'">Register</button>
             </div>
             <div class="content-up">
                 <p>SwapMoto is a platform that allows you to rent motorcycles from garages in your city.</p>
